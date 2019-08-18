fbd0a5cb12e4796844255153b5e6211e4540b419
Hannemit <hannehoitzing@gmail.com>
2019-08-18 19:22:54 +0100

    Reformat, add comments

--- Kmeans_GMM/Kmeans_utils.py
+++ Kmeans_GMM/Kmeans_utils.py
@@ -2,70 +2,69 @@ import numpy as np
 from scipy.stats import multivariate_normal
 
 
-def initialise_centers(num_clusters, data_set):
+def initialise_centres(num_clusters, data_set):
     """
-    Initialise cluster centers
+    Initialise cluster centers. Use randomly chosen datapoints as the initial cluster positions.
     :param num_clusters: integer, number of clusters
     :param data_set: dataset with each row a single example (mxn matrix)
     :return: clus, a Kxn matrix, each row is a single centroid position
     """
-    m = len(data_set)
-    rnd_idx = np.random.choice(m, size=num_clusters)  # random indices
-    clus = data_set[rnd_idx, :]  # take data points as initial centroids
-    return clus
+    rnd_idx = np.random.choice(len(data_set), size=num_clusters)  # random indices, to pick datapoints
+    cluster_coordinates = data_set[rnd_idx, :]  # take data points as initial centroids
+    return cluster_coordinates
 
 
-def get_expectation(data_set, clus):
+def get_expectation(data_set, cluster_coord):
     """
     Find closest cluster centroid to each data point
     :param data_set: mxn numpy matrix, dataset with each row a single example
-    :param clus: Kxn numpy matrix, cluster centroid coordinates with each row a single cluster position
+    :param cluster_coord: Kxn numpy matrix, cluster centroid coordinates with each row a single cluster position
     :return: idx, m-dim numpy array with cluster assignment index for each data point
-             err, float giving total error of current configuration
+             error, float giving total error of current configuration
     """
-    num_clusters = len(clus)
+    num_clusters = len(cluster_coord)
     num_train_examples = len(data_set)
 
     mat = np.zeros((num_train_examples, num_clusters))
     for i in range(num_clusters):
-        sum_sq_distance = np.sum((data_set - clus[i])**2, axis=1)
+        sum_sq_distance = np.sum((data_set - cluster_coord[i])**2, axis=1)
         mat[:, i] = sum_sq_distance
     idx_closest_cluster = np.argmin(mat, axis=1)
-    err = np.sum(mat[range(num_train_examples), idx_closest_cluster])
+    error = np.sum(mat[range(num_train_examples), idx_closest_cluster])
 
-    return idx_closest_cluster, err
+    return idx_closest_cluster, error
 
 
 def find_centers(data_set, idx, num_clusters):
     """
     Find position of cluster centroids by computing means of the data points assigned to each centre.
-    param data_set: dataset with each row a single example (mxn matrix)
+    param data_set: data set with each row a single example (mxn matrix)
     param idx: array with cluster assignment index for each data point (m-dim)
     param num_clusters: integer, number of clusters
     returns:
-        clus: 2d numpy array, cluster centroid coordinates, each row single centroid position (Kxn matrix)
+        cluster_coord: 2d numpy array, cluster centroid coordinates, each row single centroid position (Kxn matrix)
     """
     n = data_set.shape[1]
-    clus = np.zeros((num_clusters, n))
+    cluster_coord = np.zeros((num_clusters, n))
     for i in range(num_clusters):
         rows = data_set[idx == i]
-        clus[i, :] = np.mean(rows, axis=0)
-    return clus
+        cluster_coord[i, :] = np.mean(rows, axis=0)
+    return cluster_coord
 
 
 def get_responsibility(data_set, mu, cov, pi):
     """
-    Calculate responsibility matrix, r_ik, giving prob that datapoint i belongs to cluster k
-    param data_set: (m x n) matrix, dataset with each row a single example
+    Calculate responsibility matrix, r_ik, giving prob that data point i belongs to cluster k
+    param data_set: (m x n) matrix, data set with each row a single example
     param mu: (K x n) matrix, means of gaussian base dists, each row one dist
     param cov: (K x n x n) matrix, cov[k] is cov matrix of cluster k 
     param pi: k-dim array, weights of each of the clusters    
     
     Returns r: (m x K) matrix, responsibility matrix
     """
-    m = data_set.shape[0]
+    num_datapoints = data_set.shape[0]  # num_datapoints = m = num rows in data_set
     num_classes = len(pi)
-    r = np.zeros((m, num_classes))
+    r = np.zeros((num_datapoints, num_classes))
 
     for k in range(num_classes):
         normal = multivariate_normal.pdf(data_set, mean=mu[k], cov=cov[k])
@@ -77,29 +76,29 @@ def get_responsibility(data_set, mu, cov, pi):
 def do_maximisation_step(resp_mat, data_set):
     """
     Finds the mu, cov and pi (weights) that maximise the data log likelihood
-    param resp_mat: (m x K) matrix, responsibility matrix (K = num_classes)
-    param data_set: (m x n) matrix, dataset with each row a single example
+    :param resp_mat: (m x K) matrix, responsibility matrix (K = num_classes, m = num of datapoints)
+    :param data_set: (m x n) matrix, dataset with each row a single example (m = num datapoints, n = num features)
     
-    returns: 
+    :returns:
         pi:  k-dim array, weights of each of the clusters 
         mu:  (K x n) matrix, means of gaussian base dists, each row one dist
         cov: (K x n x n) matrix, cov[k] is cov matrix of cluster k 
     """
     m, num_classes = resp_mat.shape
-    n = data_set.shape[1]
+    num_features = data_set.shape[1]  # num_features = n
     
     pi = np.mean(resp_mat, axis=0)
-    mu = np.zeros((num_classes, n))
-    cov = np.zeros((num_classes, n, n))
+    mu = np.zeros((num_classes, num_features))
+    cov = np.zeros((num_classes, num_features, num_features))
     
     for k in range(num_classes):
         r_k = np.sum(resp_mat[:, k])  # r_k is a scalar
-        mu[k, :] = np.sum(resp_mat[:, k].reshape(m, 1)*data_set, axis=0)/r_k
-        mu_dot = np.dot(mu[k, :].reshape(n, 1), mu[k, :].reshape(1, n))  # mu col vec times mu row vec , nxn
+        mu[k, :] = np.sum(resp_mat[:, k].reshape(m, 1)*data_set, axis=0) / r_k
+        mu_dot = np.dot(mu[k, :].reshape(num_features, 1), mu[k, :].reshape(1, num_features))  # mu col times row vec
         
         temp = np.einsum('ab,ac->abc', data_set, data_set)
         temp2 = np.einsum('a,abc->bc', resp_mat[:, k], temp)
-        cov[k] = temp2/r_k - mu_dot  # cov is nxn matrix
+        cov[k] = temp2/r_k - mu_dot  # covariance matrix is nxn matrix
     
     return pi, mu, cov
 
@@ -117,11 +116,10 @@ def multivariate_gaussian(pos, mu, sigma):
     n = mu.shape[0]
     sigma_det = np.linalg.det(sigma)
     sigma_inv = np.linalg.inv(sigma)
-    n = np.sqrt((2*np.pi)**n * sigma_det)
+    n = np.sqrt((2 * np.pi)**n * sigma_det)
 
-    # This einsum call calculates (x-mu)T.Sigma-1.(x-mu) in a vectorized
-    # way across all the input variables.
-    fac = np.einsum('...k,kl,...l->...', pos-mu, sigma_inv, pos-mu)
+    # This einsum call calculates (x-mu)T.Sigma-1.(x-mu) in a vectorized way across all the input variables.
+    fac = np.einsum('...k,kl,...l->...', pos - mu, sigma_inv, pos - mu)
 
     return np.exp(-fac / 2.0) / n
 
@@ -139,14 +137,14 @@ def get_log_llh(resp_mat, data_set, mu, cov, pi):
         log_llh: float, expected complete data log likelihood
                  Eq. based on 'Machine learning: a probabilistic  perspective', Murphy
     """
-    llh_1 = np.sum(resp_mat*np.log(pi))
-    llh_2 = 0
+    log_llh_1 = np.sum(resp_mat*np.log(pi))
+    log_llh_2 = 0
     num_classes = len(pi)
     for k in range(num_classes):
         normal_pdf = multivariate_normal.pdf(data_set, mean=mu[k], cov=cov[k])
-        llh_2 += np.sum(resp_mat[:, k]*np.log(normal_pdf))  # .pdf(data_set) takes pdf value per row
+        log_llh_2 += np.sum(resp_mat[:, k] * np.log(normal_pdf))  # .pdf(data_set) takes pdf value per row
 
-    log_llh = llh_1 + llh_2
+    log_llh = log_llh_1 + log_llh_2
     return log_llh
 
 
@@ -163,28 +161,29 @@ def plot_2d_gaussian_contours(mu, cov, data, axs, cmap='winter', alpha=0.3):
     returns: a plot
     """
 
-    xmin, ymin = np.min(data, axis=0)
-    xmax, ymax = np.max(data, axis=0)
+    x_min, y_min = np.min(data, axis=0)
+    x_max, y_max = np.max(data, axis=0)
 
     # make grid to plot contours for multivariate Gaussian
-    xs = np.arange(xmin-1, xmax+1, 0.1)
-    ys = np.arange(ymin-1, ymax+1, 0.1)
+    xs = np.arange(x_min - 1, x_max + 1, 0.1)
+    ys = np.arange(y_min - 1, y_max + 1, 0.1)
     xs, ys = np.meshgrid(xs, ys)
 
     pos = np.empty(xs.shape + (2,))
     pos[:, :, 0] = xs
     pos[:, :, 1] = ys
 
+    # plot multivariate gaussian
     z = multivariate_gaussian(pos, mu=mu, sigma=cov)
     axs.contour(xs, ys, z, cmap=cmap, alpha=alpha)
 
 
 def normalise_data(data):
     """
-    Normalise data, subtract mean and divide by std
+    Normalise data, subtract mean and divide by standard deviation
     :param data: 2D numpy array
-    :return: normalised data
+    :return: data: 2D numpy array which is now normalised
     """
     for i in range(data.shape[1]):
-        data[:, i] = (data[:, i] - np.mean(data[:, i]))/np.std(data[:, i], ddof=1)
+        data[:, i] = (data[:, i] - np.mean(data[:, i])) / np.std(data[:, i], ddof=1)
     return data
